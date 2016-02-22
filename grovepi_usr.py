41cc63543a73f94c9066ae0f84b29b3fe32383f1
Jamie Bailey <jamie@Jamies-MacBook-Pro-2.local>
2016-02-21 18:14:29 -0600

    Fixed mixed up source files

--- grovepi_usr.py
+++ grovepi_usr.py
@@ -1,45 +1,27 @@
 import time
 import grovepi
 from ISStreamer.Streamer import Streamer
-
+  
 # --------- User Settings ---------
 # Connect the ultrasonic ranger sensor to one of the digital pins (i.e. 2, 3, 4, 7, or 8)
-PIR_SENSOR_PIN = 8
-ROOM_NAME = "Office"
 USR_SENSOR_PIN = 4
 OBJECT_NAME = "Cookies"
 OBJECT_EMOJI_TOKEN = ":cookie:"
 # Initial State settings
-BUCKET_NAME = ROOM_NAME + " Log"
-BUCKET_KEY = "usrpir20"
-ACCESS_KEY = "PLACE YOUR INITIAL STATE ACCESS KEY HERE"
+BUCKET_NAME = OBJECT_NAME + " Log"
+BUCKET_KEY = "pir022016"
+ACCESS_KEY = "DRb9ze9iI3LATtFuAWEiRxOYwi3S4bzE"
 # Set the time between sensor reads
 SECONDS_BETWEEN_READS = .2
 # ---------------------------------
   
-grovepi.pinMode(PIR_SENSOR_PIN,"INPUT")
 streamer = Streamer(bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=ACCESS_KEY)
-lastValue = "-"
 proximity = 1000
 lastProximity = 0
 compromised = False
  
 while True:
     try:
-        # Detect motion and log when there are changes
-        if grovepi.digitalRead(PIR_SENSOR_PIN):
-            if lastValue != "active":
-                lastValue = "active"
-                streamer.log (ROOM_NAME + " Motion", lastValue)
-                streamer.flush()
-                print 'Motion Detected'
-        else:
-            if lastValue != "inactive":
-                lastValue = "inactive"
-                streamer.log (ROOM_NAME + " Motion", lastValue)
-                streamer.flush()
-                print '-'
- 
         # If distance value from Ultrasonic is less than 60 cm, log it
         proximity = grovepi.ultrasonicRead(USR_SENSOR_PIN)
         if proximity < 60:
